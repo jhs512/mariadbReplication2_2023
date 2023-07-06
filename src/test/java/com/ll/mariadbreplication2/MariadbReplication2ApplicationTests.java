a0160f70c7fc9162e0674da31f72ff3e33ae6ed5
jhs512 <jangka512@gmail.com>
2023-07-06 09:37:13 +0900

    세팅

---
+++ src/test/java/com/ll/mariadbreplication2/MariadbReplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mariadbreplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariadbReplication2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
